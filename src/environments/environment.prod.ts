5eaf83e1570dc436d4f4e8bba9e64af8ef2a3d18
Daniel Rogowski <daniel.rogowski@onet.pl>
2023-03-18 11:20:18 +0100

    🔧 Differentiate prod/dev enviroments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,10 @@
 export const environment = {
   shelfSpace: {
-    apiEndpoint: "http://localhost:8080"
+    apiEndpoint: "https://4f9mqd3c87.execute-api.eu-central-1.amazonaws.com/prod"
   },
   oauth2: {
     clientId: "2i7lm8qa38nrh9j86mk19i9fg5",
-    redirectUrl: "http://localhost:4200/auth/oauth2",
+    redirectUrl: "https://shelfspace.danielrogowski.net/auth/oauth2",
     authEndpoint: "https://auth.shelfspace.danielrogowski.net/login",
     authTokenEndpoint: "https://auth.shelfspace.danielrogowski.net/oauth2/token"
   }
